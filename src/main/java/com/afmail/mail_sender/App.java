0dbddd0a51500c5ff6e85ec1157615a3b4a77a17
Alexandre Fernandes <alex.silva250@hotmail.com>
2024-05-21 21:23:59 -0300

    feat: add javamail dependency

--- src/main/java/com/afmail/mail_sender/App.java
+++ src/main/java/com/afmail/mail_sender/App.java
@@ -1,13 +1,9 @@
 package com.afmail.mail_sender;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+   
+	
+	public void testEmail() {
+		
+	}
 }
