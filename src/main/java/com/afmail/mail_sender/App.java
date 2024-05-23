eee85581ee758527076fed8949db8ba3a4bb3b00
Alexandre Fernandes <alex.silva250@hotmail.com>
2024-05-22 22:13:03 -0300

    feat: initial properties

--- src/main/java/com/afmail/mail_sender/App.java
+++ src/main/java/com/afmail/mail_sender/App.java
@@ -1,9 +1,16 @@
 package com.afmail.mail_sender;
 
+import java.util.Properties;
+
 public class App {
    
-	
 	public void testEmail() {
-		
+		Properties properties = new Properties();
+		properties.put("mail.smtp.auth", true);
+		properties.put("mail.smtp.starttls", "true");
+		properties.put("mail.smtp.host", "smtp.gmail.com");
+		properties.put("mail.smtp.port", "465");
+		properties.put("mail.smtp.socketFactory.port", "465");
+		properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 	}
 }
